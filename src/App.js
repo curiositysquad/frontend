960a26527ac9a41aafd9c228431476a7132c1f05
Peter Gacek <peterwgacek@gmail.com>
2021-01-07 12:11:59 -0500

    modifies app.js to working

--- src/App.js
+++ src/App.js
@@ -4,28 +4,17 @@ import Header from './components/Header';
 import Main from './components/Main';
 import './App.css';
 
+
 function App() {
-  const [TodoListState, setTodoListState] = useState({ todos: [] });
-
-  useEffect(() => {
-    getTodoList;
-  }, []);
-
-  function handleAdd(event, formInputs) {
-    event.preventDefault();
-    console.log(formInputs);
-  }
-  //... and pass this into the Aside
-
-  <Aside handleSubmit={handleAdd} />
-}
-
-
-function getTodoList() {
-  fetch('/TodoItems')
-    .then(response => response.json())
-    .then(json => setTodoState({ todos: json }))
-    .catch(error => console.error(error));
+  const [todosState, setTodoListState] = useState({ notices: [] });
+  return (
+    <div className="App">
+      <div className='container'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
