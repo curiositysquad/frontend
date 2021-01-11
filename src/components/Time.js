c80cc34cc315a1e00bba528be6d64c16fc43fd57
Peter Gacek <peterwgacek@gmail.com>
2021-01-11 11:55:42 -0500

    Pushing to heroku

---
+++ src/components/Time.js
@@ -0,0 +1,28 @@
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
+
+
+// class App extends Component {
+
+//     constructor() {
+//         var today = new Date(),
+//             time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+//         this.state = {
+//             currentTime: time
+//         }
+//     }
+
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <p>
+//                 {this.state.currentTime}
+//                 </p>
+//             </div>
+//         );
+//     }
+// }
+
+// render(<App />, document.getElementById('root'));
