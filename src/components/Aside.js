d78cbf07a2dd2bc3e895d13c807555c82b7bb76e
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-13 15:46:23 -0800

    adds DateTime, City

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,10 +1,12 @@
 import Form from './Form.js';
 import './styles/Aside.css'
+import DateTime from './DateTime'
 
 function Aside(props) {
     return (
         <aside className="Aside">
-            <h1>What is your main focus for today?</h1>
+            <DateTime />
+            {/* <h1>What is your main focus for today?</h1> */}
             <Form handleSubmit={props.handleSubmit} />
         </aside>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import './styles/Footer.css'
 export default function Footer(props) {
     return (
         <footer className="Footer">
-            <p>Copyright &copy; Morementum {new Date().getFullYear()} All Rights Reserved </p>
+            <p>"Believe you can and you're halfway there."</p>
+            {/* <p>Copyright &copy; Morementum {new Date().getFullYear()} All Rights Reserved </p> */}
 
         </footer>
     );
